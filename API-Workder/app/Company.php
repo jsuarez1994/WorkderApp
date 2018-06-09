d761b23f907f087d729fcb57e80e022ab5c614dd
jsuarez1994 <jesus1994sl@gmail.com>
2018-06-09 13:37:46 +0200

    Comentario Company.php

--- API-Workder/app/Company.php
+++ API-Workder/app/Company.php
@@ -1,7 +1,7 @@
 <?php
 namespace App;
 use Illuminate\Database\Eloquent\Model;
-
+//Entidad de la tabla Company
 class Company extends Model {
     protected $table = 'company';
     public $timestamps = false;
